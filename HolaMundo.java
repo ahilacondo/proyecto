5e069f34388ab41e48b2fbe6ae1014bb2591c13d
ahilacondo <ahilacondo@unsa.edu.pe>
2023-05-06 13:37:35 -0500

    Pide nombre y lo imprime

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class HolaMundo {
   public static void main(String args[]){
+    Scanner scn = new Scanner(System.in);
+
     System.out.println("Hola Mundo");
     System.out.println("Bienvenido al curso de EDA");
+    System.out.print("Ingrese su nombre: ");
+    String name= scn.nextLine();
+    System.out.println(name);
   }
   
 
